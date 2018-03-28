8f23de4f8281478a8a234bc10436614cc799f16c
akhant <a.khantelev@mail.ru>
2018-03-28 17:22:18 +0300

    secret

--- config/secret.js
+++ config/secret.js
@@ -3,8 +3,8 @@ export default {
     port: 3000,
     secretKey: 'banana2018',
     facebook: {
-        clientID: process.env.FACEBOOK_ID || '' ,
-        clientSecret: process.env.FACEBOOK_SECRET || '',
+        clientID: process.env.FACEBOOK_ID || '1784325955202755' ,
+        clientSecret: process.env.FACEBOOK_SECRET || '0e6abeed33d177ffb74d1eca44edaf1f',
         profileFields: ['emails', 'displayName'],
         callbackURL: 'http://localhost:3000/auth/facebook/callback'
     }
